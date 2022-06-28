f150cdb73faf67a1f2f476d61895288a6aecd521
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2022-06-28 13:33:33 -0300

    One Poinjt Seven

---
+++ ArrayAndStrings/OnePointSeven.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrackTheCodeInterview.ArrayAndStrings
+{
+    public class OnePointSeven
+    {
+
+        public string[][] RotateMatrix(string[][] matrix)
+        {
+            string[][] outPutArray = new string[matrix.Length][];
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int column = 0; column < matrix[row].Length; column++)
+                {
+                    int novaColuna = (matrix[row].Length -1) - row;
+                    int novaLinha = column;
+                    if (outPutArray[novaLinha] == null)
+                        outPutArray[novaLinha] = new string[matrix[row].Length];
+                    outPutArray[novaLinha][novaColuna] = matrix[row][column];
+                }
+            }
+            return outPutArray;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,11 +8,33 @@ namespace CrackTheCodeInterview
     {
         static void Main()
         {
-            var onePointFive = new OnePointSix();
+            var onePointSeven = new OnePointSeven();
+            string[][] matrix = new string[][]
+            {
+                new string[] { "A", "B", "C", "D" },
+                new string[] { "E", "F", "G", "H" },
+                new string[] { "I", "L", "M", "N" },
+                new string[] { "O", "P", "Q", "R" },
+            };
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int column = 0; column < matrix[row].Length; column++)
+                {
+                    Console.Write(matrix[row][column]);
+                }
+                Console.WriteLine("");
+            }
+                Console.WriteLine("");
 
-
-            Console.WriteLine(onePointFive.CompressString("aabcccccaaa"));
-
+            var result = onePointSeven.RotateMatrix(matrix);
+            for (int row = 0; row < result.Length; row++)
+            {
+                for (int column = 0; column < result[row].Length; column++)
+                {
+                    Console.Write(result[row][column]);
+                }
+                Console.WriteLine("");
+            }
             Console.ReadKey();
         }
     }

74cf0fceaa56597b33d816bfa846b314829bfb68
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2022-07-20 19:20:32 -0300

    Stacks

--- DataStructures/MyStack.cs
+++ DataStructures/MyStack.cs
@@ -18,7 +18,7 @@ namespace CrackTheCodeInterview.DataStructures
 
         public void Push(T item)
         {
-            if(_top == _max - 1)
+            if (_top == _max - 1)
             {
                 throw new StackOverflowException();
             }
@@ -60,6 +60,151 @@ namespace CrackTheCodeInterview.DataStructures
                 sb.AppendLine(item.ToString());
             }
             return sb.ToString();
+        }
+    }
+
+    public class MyStack2OneArray<T>
+    {
+        private T[] _elements;
+        private int _topLeft;
+        private int _topRight;
+        private int _size;
+
+        public MyStack2OneArray(int size)
+        {
+            _size = size;
+            _elements = new T[size];
+            _topLeft = -1;
+            _topRight = size;
+        }
+
+        public void Push(T element, int stack = 1)
+        {
+            if (stack == 1)
+            {
+                if (_topLeft < _topRight - 1)
+                {
+                    _topLeft++;
+                    _elements[_topLeft] = element;
+                }
+                else
+                    throw new StackOverflowException();
+            }
+            else
+            {
+                if (_topLeft < _topRight - 1)
+                {
+                    _topRight--;
+                    _elements[_topRight] = element;
+                }
+                else
+                    throw new StackOverflowException();
+            }
+        }
+
+        public T Pop(int stack)
+        {
+            if (stack == 1)
+            {
+                if (_topLeft >= 0)
+                {
+                    var element = _elements[_topLeft];
+                    _topLeft--;
+                    return element;
+                }
+                else
+                {
+                    throw new Exception("Stack is Empty");
+                }
+            }
+            else
+            {
+                if (_topRight < _size)
+                {
+                    var element = _elements[_topRight];
+                    _topRight++;
+                    return element;
+                }
+                else
+                {
+                    throw new Exception("Stack is Empty");
+                }
+            }
+        }
+    }
+
+    public class MyStackNOneArray<T>
+    {
+        private T[] _elements;
+        private int[] _tops;
+        private int[] _next;
+
+        private int _size;
+        private int _totalStacks;
+        private int _free;
+
+
+        public MyStackNOneArray(int totalStacks, int arraySize)
+        {
+            _totalStacks = totalStacks;
+            _size = arraySize;
+            _elements = new T[_size];
+            _tops = new int[_totalStacks];
+            _next = new int[_size];
+            for (int i = 0; i < _totalStacks; i++)
+            {
+                _tops[i] = -1;
+            }
+
+            _free = 0;
+            for (int i = 0; i < _size - 1; i++)
+            {
+                _next[i] = i + 1;
+            }
+            _next[_size - 1] = -1;
+        }
+
+        public virtual bool Full
+        {
+            get
+            {
+                return (_free == -1);
+            }
+        }
+
+        public void Push(T item, int sn)
+        {
+            if (Full)
+                throw new StackOverflowException();
+
+            int i = _free;
+
+            _free = _next[i];
+
+            _next[i] = _tops[sn];
+            _tops[sn] = i;
+
+            _elements[i] = item;
+        }
+
+        public T Pop(int sn)
+        {
+            if (isEmpty(sn))
+                throw new Exception("Stack is Empty");
+
+            int i = _tops[sn];
+
+            _tops[sn] = _next[i];
+
+            _next[i] = _free;
+            _free = i;
+
+            return _elements[i];
+        }
+
+        private bool isEmpty(int sn)
+        {
+            return (_tops[sn] == -1);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,20 +9,45 @@ namespace CrackTheCodeInterview
     {
         static void Main()
         {
-            MyStack<string> stack = new MyStack<string>(5);
+            MyStack2OneArray<string> stack = new MyStack2OneArray<string>(10);
             stack.Push("A");
             stack.Push("B");
             stack.Push("C");
             stack.Push("D");
             stack.Push("E");
 
-            Console.WriteLine(stack);
+            stack.Push("F",2);
+            stack.Push("G",2);
+            stack.Push("H",2);
+            stack.Push("I",2);
+            stack.Push("J",2);
+
+
             
-            Console.WriteLine(stack.Pop());
+            Console.WriteLine(String.Format("Top Element from Stack 1 is  : {0}", stack.Pop(1)));
+            Console.WriteLine(String.Format("Top Element from Stack 2 is  : {0}", stack.Pop(2)));
 
             Console.WriteLine("*************************");
 
 
+            MyStackNOneArray<int> stackNOneArray = new MyStackNOneArray<int>(3, 10);
+
+            stackNOneArray.Push(1,0);
+            stackNOneArray.Push(2,0);
+            stackNOneArray.Push(3,0);
+
+            stackNOneArray.Push(4, 1);
+            stackNOneArray.Push(5, 1);
+            stackNOneArray.Push(6, 1);
+
+            stackNOneArray.Push(7, 2);
+            stackNOneArray.Push(8, 2);
+            stackNOneArray.Push(9, 2);
+            stackNOneArray.Push(10, 2);
+
+            Console.WriteLine(String.Format("Top Element from Stack 1 is  : {0}", stackNOneArray.Pop(0)));
+            Console.WriteLine(String.Format("Top Element from Stack 2 is  : {0}", stackNOneArray.Pop(1)));
+            Console.WriteLine(String.Format("Top Element from Stack 3 is  : {0}", stackNOneArray.Pop(2)));
 
             Console.ReadKey();
         }

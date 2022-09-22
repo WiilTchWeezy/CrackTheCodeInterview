ea09e9417f2ee8e5a564fa22e4cbc77909538555
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2022-09-22 20:31:33 -0300

    Adding Book Solutions

--- ArrayAndStrings/OnePointTwo.cs
+++ ArrayAndStrings/OnePointTwo.cs
@@ -23,5 +23,27 @@ namespace CrackTheCodeInterview.ArrayAndStrings
             else
                 return false;
         }
+
+        //My solution follow the same type of thought but i only do one for and try to sum the ASCII values
+        public bool IsPermutationBookSolution(string a, string b)
+        {
+            if (a.Length != b.Length)
+                return false;
+
+            int[] letters = new int[128];
+
+            foreach (var aChar in a)
+            {
+                letters[aChar]++;
+            }
+
+            foreach (var bChar in b)
+            {
+                letters[bChar]--;
+                if (letters[bChar] < 0)
+                    return false;
+            }
+            return true;
+        }
     }
 }

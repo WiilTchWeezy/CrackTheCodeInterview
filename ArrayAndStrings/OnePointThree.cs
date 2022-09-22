4788dea2275680bf28693850b6b0688cca1d6bb9
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2022-09-22 20:42:42 -0300

    Adding Book Solution to OnePointThree

--- ArrayAndStrings/OnePointThree.cs
+++ ArrayAndStrings/OnePointThree.cs
@@ -32,5 +32,36 @@ namespace CrackTheCodeInterview.ArrayAndStrings
             }
             return new String(charArray);
         }
+
+        //That works with the string in backwards but it's very similar to my solution
+        public void ReplaceStringBookSolution(char[] str, int trueLength)
+        {
+            int spaceCount = 0, index, i = 0;
+            for (i = 0; i < trueLength; i++)
+            {
+                if(str[i] == ' ')
+                {
+                    spaceCount++;
+                }
+            }
+            index = trueLength + spaceCount * 2;
+            if (trueLength < str.Length)
+                str[trueLength] = '\0';
+            for (i = trueLength -1; i >= 0; i--)
+            {
+                if(str[i] == ' ')
+                {
+                    str[index - 1] = '0';
+                    str[index - 2] = '2';
+                    str[index - 3] = '%';
+                    index = index - 3;
+                }
+                else
+                {
+                    str[index - 1] = str[i];
+                    index--;
+                }
+            }
+        }
     }
 }

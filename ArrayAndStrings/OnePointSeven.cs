a75083c69c501a2f6645bda556963f03546e1803
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2022-09-28 18:56:49 -0300

    Adding Book Solution to OnePointSeven

--- ArrayAndStrings/OnePointSeven.cs
+++ ArrayAndStrings/OnePointSeven.cs
@@ -6,7 +6,7 @@ namespace CrackTheCodeInterview.ArrayAndStrings
 {
     public class OnePointSeven
     {
-
+        //BIG O -> O(N²)
         public string[][] RotateMatrix(string[][] matrix)
         {
             string[][] outPutArray = new string[matrix.Length][];
@@ -23,5 +23,37 @@ namespace CrackTheCodeInterview.ArrayAndStrings
             }
             return outPutArray;
         }
+
+        #region BookSolution
+        //BIG O -> O(N²)
+        public bool Rotate(int[][] matrix)
+        {
+            if (matrix.Length == 0 || matrix.Length != matrix[0].Length)
+                return false;
+            int n = matrix.Length;
+            for (int layer = 0; layer < n / 2; layer++)
+            {
+                int first = layer;
+                int last = n - 1 - layer;
+                for (int i = first; i < last; i++)
+                {
+                    int offset = i - first;
+                    int top = matrix[first][i]; //save top
+                    //left -> top
+                    matrix[first][i] = matrix[last - offset][first];
+
+                    //bottom -> left
+                    matrix[last - offset][first] = matrix[last][last - offset];
+
+                    //right -> bottom
+                    matrix[last][last - offset] = matrix[i][last];
+
+                    //top -> right
+                    matrix[i][last] = top;
+                }
+            }
+            return true;
+        } 
+        #endregion
     }
 }

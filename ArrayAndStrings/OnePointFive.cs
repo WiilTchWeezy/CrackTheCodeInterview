11d791f0d80e564db57ebabe9f8a8bbf2e7e4877
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2022-09-27 23:15:02 -0300

    Adding Book Solution to OnePointFive

--- ArrayAndStrings/OnePointFive.cs
+++ ArrayAndStrings/OnePointFive.cs
@@ -7,6 +7,7 @@ namespace CrackTheCodeInterview.ArrayAndStrings
     public class OnePointFive
     {
         //BIG O O(N log N) where N is the longest string
+        #region SolutionByTry
         public bool OneOrZeroEditsLeft(string inputA, string inputB)
         {
             if (inputA.Equals(inputB))
@@ -18,7 +19,7 @@ namespace CrackTheCodeInterview.ArrayAndStrings
             if (lenghtDiff > 1)
                 return false;
             int charDiff = 0;
-            if(inputA.Length == inputB.Length)
+            if (inputA.Length == inputB.Length)
             {
                 int totalDiff = 0;
                 for (int i = 0; i < inputA.Length; i++)
@@ -67,13 +68,13 @@ namespace CrackTheCodeInterview.ArrayAndStrings
             char[] inputCharArray = inputString.ToCharArray();
             for (int i = 0; i < outputChar.Length; i++)
             {
-                if(i == index)
+                if (i == index)
                 {
                     outputChar[i] = insertChar;
                 }
                 else if (i > index)
                 {
-                    outputChar[i] = inputCharArray[i-1];
+                    outputChar[i] = inputCharArray[i - 1];
                 }
                 else
                 {
@@ -82,5 +83,61 @@ namespace CrackTheCodeInterview.ArrayAndStrings
             }
             return new string(outputChar);
         }
+        #endregion
+
+        #region BookSolution
+        public bool OneEditAway(string first, string second)
+        {
+            if (first.Length == second.Length)
+            {
+                return OneEditReplace(first, second);
+            }
+            else if (first.Length + 1 == second.Length)
+            {
+                return OneEditInsert(first, second);
+            }
+            else if (first.Length - 1 == second.Length)
+            {
+                return OneEditInsert(second, first);
+            }
+            return false;
+        }
+
+        public bool OneEditReplace(string s1, string s2)
+        {
+            bool foundDiff = false;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] != s2[i])
+                {
+                    if (foundDiff)
+                        return false;
+                    foundDiff = true;
+                }
+            }
+            return true;
+        }
+
+        public bool OneEditInsert(string s1, string s2)
+        {
+            int index1 = 0;
+            int index2 = 0;
+            while (index2 < s2.Length && index1 < s1.Length)
+            {
+                if (s1[index1] != s2[index2])
+                {
+                    if (index1 != index2)
+                        return false;
+                    index2++;
+                }
+                else
+                {
+                    index1++;
+                    index2++;
+                }
+            }
+            return true;
+        } 
+        #endregion
     }
 }

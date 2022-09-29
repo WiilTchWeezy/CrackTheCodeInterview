48834fb44af761a2af9f5eb4f215de22beab1884
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2022-09-29 19:11:39 -0300

    Adding Last Solutions To Array and Strings

--- ArrayAndStrings/OnePointEight.cs
+++ ArrayAndStrings/OnePointEight.cs
@@ -38,5 +38,53 @@ namespace CrackTheCodeInterview.ArrayAndStrings
             }
             return outPutArray;
         }
+
+        #region BookSolutionOne
+        public void SetZeros(int[][] matrix)
+        {
+            bool[] row = new bool[matrix.Length];
+            bool[] column = new bool[matrix[0].Length];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        row[i] = true;
+                        column[j] = true;
+                    }
+                }
+            }
+
+            for (int i = 0; i < row.Length; i++)
+            {
+                if (row[i])
+                    SetRowToZeros(matrix, i);
+            }
+            for (int i = 0; i < column.Length; i++)
+            {
+                if (column[i])
+                    SetColumnsToZeros(matrix, i);
+            }
+
+        }
+        void SetRowToZeros(int[][] matrix, int row)
+        {
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                matrix[row][i] = 0;
+            }
+        }
+
+        void SetColumnsToZeros(int[][] matrix, int column)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i][column] = 0;
+            }
+        } 
+        #endregion
+
+
     }
 }
---
+++ ArrayAndStrings/OnePointNine.cs
@@ -0,0 +1,20 @@
+﻿namespace CrackTheCodeInterview.ArrayAndStrings
+{
+    public class OnePointNine
+    {
+
+        public bool IsRotation(string s1, string s2)
+        {
+            if(s1.Length == s2.Length && s1.Length > 0)
+            {
+                string s1s1 = s1 + s1;
+                return IsSubString(s1s1, s2);
+            }
+            return false;
+        }
+        public bool IsSubString(string s1, string s2)
+        {
+            return s1.Contains(s2);
+        }
+    }
+}

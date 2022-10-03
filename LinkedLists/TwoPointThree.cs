c33554b9d6249eb31512b753e5994a4fa0b4af5c
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2022-10-03 20:10:32 -0300

    Adding BookSolution To Two Point Three

---
+++ LinkedLists/TwoPointThree.cs
@@ -0,0 +1,21 @@
+﻿using CrackTheCodeInterview.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrackTheCodeInterview.LinkedLists
+{
+    public class TwoPointThree
+    {
+        public bool DeleteNode(Node<int> n)
+        {
+            if (n == null || n.Next == null)
+                return false;
+
+            Node<int> next = n.Next;
+            n.Data = next.Data;
+            n.Next = next.Next;
+            return true;
+        }
+    }
+}
